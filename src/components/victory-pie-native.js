3da04a7a851dd140f760f66f70467a46be0b8ca9
Lauren <lauren.eastridge@formidablelabs.com>
2016-06-20 21:46:14 -0700

    component renders but page position is weird

---
+++ src/components/victory-pie-native.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { G } from "react-native-svg";
+import { default as VictoryPie } from "./victory-pie";
+import { default as Slice } from "./native-slice";
+import { VictoryLabelNative, VictoryContainerNative } from "victory-core";
+
+export default class extends VictoryPie {
+  static defaultProps = Object.assign(
+    VictoryPie.defaultProps,
+    {
+      dataComponent: <Slice />,
+      groupComponent: <G/>,
+      labelComponent: <VictoryLabelNative/>,
+      containerComponent: <VictoryContainerNative/>
+    }
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import VictoryPie from "./components/victory-pie";
-export { VictoryPie };
+export { default as VictoryPie } from "./components/victory-pie";
+export { default as VictoryPieNative } from "./components/victory-pie-native";
